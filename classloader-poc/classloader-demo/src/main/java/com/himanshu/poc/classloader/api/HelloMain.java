d24e2d7d63f7dd4166fa7a46f6c1a25880632cb8
himanshu bhardwaj <himanshu_bhardwajin@yahoo.co.in>
2018-03-16 19:55:09 +0530

    Added classloader implementation.

--- classloader-poc/classloader-demo/src/main/java/com/himanshu/poc/classloader/api/HelloMain.java
+++ classloader-poc/classloader-demo/src/main/java/com/himanshu/poc/classloader/api/HelloMain.java
@@ -8,16 +8,21 @@ public class HelloMain {
     String path = HelloMain.class.getResource("/").getFile();
     File currentPath = new File(path);
     File projectRoot = currentPath.getParentFile().getParentFile().getParentFile();
+
     String impl1Path = projectRoot.getPath()+"/extension-1/target/";
-    String impl2Path = projectRoot.getPath()+"/extension-2/target/";
-
-    HelloServiceClassloader impl1Loader = new HelloServiceClassloader(new File(impl1Path), f -> f.getName().endsWith("jar"));
-    HelloServiceClassloader impl2Loader = new HelloServiceClassloader(new File(impl2Path), f -> f.getName().endsWith("jar"));
-
+    HelloServiceClassloader impl1Loader = new HelloServiceClassloader(new File(impl1Path), HelloMain::isJarFile);
     IHelloService helloService1 = impl1Loader.resolveClass("com.himanshu.poc.classloader.impl.v1.HelloService");
-    IHelloService helloService2 = impl2Loader.resolveClass("com.himanshu.poc.classloader.impl.v2.HelloService");
-
     System.out.println(helloService1.sayHello());
+
+    String impl2Path = projectRoot.getPath()+"/extension-2/target/";
+    HelloServiceClassloader impl2Loader = new HelloServiceClassloader(new File(impl2Path), HelloMain::isJarFile);
+    IHelloService helloService2 = impl2Loader.resolveClass("com.himanshu.poc.classloader.impl.v2.HelloService");
     System.out.println(helloService2.sayHello());
+
+
+  }
+
+  private static boolean isJarFile(File f) {
+    return f.getName().endsWith("jar");
   }
 }
